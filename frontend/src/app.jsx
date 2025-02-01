84a419787e33452ecd0d50b3eca32cfe671f3208
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2025-02-01 10:15:02 +0530

    navbar added

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,17 +1,22 @@
-import logInPopUp from './components/logInPopUp/logInPopUp'
+//import logInPopUp from './components/logInPopUp/logInPopUp'
+import Navbar from "./components/NavBar/Navbar";
+
 
 
 import './app.css'
 
 export function App() {
-  const[showLogin,setShowLogin]=useState(false);
+  // const[showLogin,setShowLogin]=useState(false);
 
   return (
     <>
-        {showLogin?<logInPopup/>:<></>}
-        <div className='app'>
-          <Navbar setShowLogin={setShowLogin} />
-          <p>hii</p>
+        <div className="app">
+          <Navbar></Navbar>
+          {/* {showLogin?<logInPopup/>:<></>}
+          <div className='app'>
+            <Navbar setShowLogin={setShowLogin} />
+            <p>hii</p>
+          </div> */}
         </div>
     </>
   )
---
+++ frontend/src/assets/food images/asset.js
@@ -0,0 +1,7 @@
+import logo from "./logo.jpg";
+import pastry from "./pexels-elli-559179-1854652.jpg";
+
+export const assets = {
+    logo,
+    pastry
+};
