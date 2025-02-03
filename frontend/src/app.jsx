41610a1271f67c94a683d9fd950dc099ffbe7233
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2025-02-04 01:18:02 +0530

    updated Home

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,22 +1,25 @@
-<<<<<<< HEAD
-//import logInPopUp from './components/logInPopUp/logInPopUp'
-import Navbar from "./components/NavBar/Navbar";
+import React from 'react';
 
+import { Route, Routes } from 'react-router-dom';
+import './app.css';
+import Navbar from './components/NavBar/Navbar';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
+import { PlaceOrder } from './pages/PlaceOrder/PlaceOrder';
 
-=======
-import logInPopUp from './components/logInPopUp/logInPopUp'
-import Navbar from 
->>>>>>> 82c6bccdbb09e97a6d25b2e3926b86b34e2b8b06
-
-import './app.css'
 
 export function App() {
-  // const[showLogin,setShowLogin]=useState(false);
+   
 
   return (
     <>
         <div className="app">
           <Navbar></Navbar>
+          <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/Cart" element={<Cart/>}></Route>
+            <Route path="/PlaceOrder" element={<PlaceOrder/>}></Route>
+          </Routes>
           {/* {showLogin?<logInPopup/>:<></>}
           <div className='app'>
             <Navbar setShowLogin={setShowLogin} />
