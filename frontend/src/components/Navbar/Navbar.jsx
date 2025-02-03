c0c02b504342fcf4bd45127a5fc19e85a3a3894c
vanshikabansal02 <166854639+vanshikabansal02@users.noreply.github.com>
2025-02-04 00:18:33 +0530

    updated

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import "./Navbar.css";
+import { assets } from "../../assets/food images/asset.js";
+import { FcSearch } from "react-icons/fc";
+import { PiHandbagFill } from "react-icons/pi";
+import {useState} from "react";
 
-const Navbar = () => {
-  return (
-    <div>
-     
-    </div>
-  )
+function Navbar(){
+
+    const[menu,setmenu]=useState("Home")
+    return(
+        <>
+            <div className="navbar">
+               <img src={assets.logo} alt="logo" className="logo"></img>
+               <ul className="nav-menu">
+                <li onClick={()=> setmenu("Home")}  className={menu==="Home"? "active":""} >Home</li>
+                <li onClick={()=> setmenu("Menu")} className={menu==="Menu"? "active":""}>Menu</li>
+                <li onClick={()=> setmenu("Mobile-app")} className={menu==="Mobile-app"? "active":""}>Mobile-app</li>
+                <li onClick={()=> setmenu("Contact us")} className={menu==="Contact us"? "active":""}>Contact us</li>  
+               </ul>
+               <div className="nav-right">
+                <div className="search">
+                    <div className="search-i"><FcSearch /></div>
+                    <div className="search-box">
+                        <input type="text" placeholder="Search for food items"></input>
+                    </div>
+                </div>
+                <div className="cart"><PiHandbagFill /></div>
+                <button>Sign in</button>
+
+               </div>
+            </div>
+        </>
+    )
 }
-
-export default Navbar
+export default Navbar;
