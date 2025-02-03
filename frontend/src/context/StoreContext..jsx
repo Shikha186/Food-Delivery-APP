55a0fd9aa743a0d7df2cb9e0b556b4a59b327c75
vanshikabansal02 <166854639+vanshikabansal02@users.noreply.github.com>
2025-02-04 01:24:05 +0530

    new place order page added

---
+++ frontend/src/context/StoreContext..jsx
@@ -0,0 +1,23 @@
+const getTotalCartAmount=()=>
+    {
+        let totalAmount=0;
+        
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0)
+            {
+                let itemInfo=food_list.find((product)=>product._id===item)
+                totalAmount+=itemInfo.price* cartItems[item];
+            }
+           
+        }
+        return totalAmount;
+    }
+    const contextValue={
+        getTotalCartAmount
+    }
+    return(
+        <StoreContext.Provider value={contextValue}>
+    {props.children}
+        
+    )
