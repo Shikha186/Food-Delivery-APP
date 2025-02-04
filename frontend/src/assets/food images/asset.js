4b160545db1fa5b5eb3b013153b25ecc9e64881d
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2025-02-04 22:39:58 +0530

    main.jsx and components updated

--- frontend/src/assets/food images/asset.js
+++ frontend/src/assets/food images/asset.js
@@ -1,7 +1,55 @@
 import logo from "./logo.jpg";
 import pastry from "./pexels-elli-559179-1854652.jpg";
+import pizzacover from "./pizza cover.avif";
+import saladcover from "./salad.jpg";
+import maincourse from "./north indian main course.jpg";
+import dessert from "./dessert.jpg";
+import southindian from "./idli.webp";
+import drinks from "./drinks.jpg";
+import continental from "./continental.jpg";
+import chicken from "./chicken.jpg";
+import sandwich from "./cheese sandwich2.jpg";
 
 export const assets = {
     logo,
     pastry
 };
+
+export const menu_head = [
+    {
+        menu_img: pizzacover,
+        menu_name: "Pizza"
+    },
+    {
+        menu_img: saladcover,
+        menu_name: "Salad"
+    },
+    {
+        menu_img:maincourse,
+        menu_name: "Main Course"
+    },
+    {
+        menu_img:dessert,
+        menu_name: "Dessert"
+    },
+    {
+        menu_img: southindian,
+        menu_name: "South Indian Cuisine"
+    },
+    {
+        menu_img: drinks,
+        menu_name: "Drinks"
+    },
+    {
+        menu_img: continental,
+        menu_name: "Continental Dishes" 
+    },
+    {
+        menu_img: chicken,
+        menu_name: "Chicken",
+    },
+    {
+        menu_img: sandwich,
+        menu_name: "Sandwich",
+    }
+]
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,12 @@
 import { render } from 'preact'
 import './index.css'
 import { App } from './app.jsx'
+import ReactDom from 'react-dom/client';
+import  {BrowserRouter} from 'react-router-dom';
 
-render(<App />, document.getElementById('app'))
+// render(<App />, document.getElementById('app'))
+ReactDom.createRoot(document.getElementById('app')).render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+)
