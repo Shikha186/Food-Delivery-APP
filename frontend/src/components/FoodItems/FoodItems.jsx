62be3c919dc68856251d8447306262c6646a9f86
Shikha186 <163021594+Shikha186@users.noreply.github.com>
2025-02-07 13:04:52 +0530

    context updated

--- frontend/src/components/FoodItems/FoodItems.jsx
+++ frontend/src/components/FoodItems/FoodItems.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import "./FoodItems.css";
 import { assets } from '../../assets/food images/asset';
 import { FaRupeeSign } from "react-icons/fa";
+import { useState } from 'react';
+import { useContext } from 'react';
+import { StoreContext } from '../../context/StoreContext.';
 
 
 function FoodItems({id,name,image,price,description}){
+  const[itemCount,setItemcount]=useState(0);
+  const{cartItems,addToCart,removeFromCart}=useContext(StoreContext);
   return (
     <div className='food-item'>
         <div className="food-item-image-container">
             <img  className="food-item-img" src={image} alt={name}/>
-
         </div>
+        
         <div className="food-item-info">
             <div className="food-item-name-rating">
                 <p>{name}</p>
@@ -18,6 +23,14 @@ function FoodItems({id,name,image,price,description}){
             </div>
             <p className='description'>{description}</p>
             <p className='price'><FaRupeeSign  className='rupee'/>{price}</p>
+            <div>
+              {!cartItems[id]?<button className="add-button" onClick={()=>addToCart(id)}>Add</button>:
+                <div className="add-button-container">
+                  <button className="add-button" onClick={()=>removeFromCart(id)}>-</button>
+                  <p className="item-count">{cartItems[id]}</p>
+                  <button className="add-button" onClick={()=>addToCart(id)}>+</button>
+                </div>}
+        </div>
         </div>
     </div>
   )
