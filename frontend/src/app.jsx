e9049f627c6891c6f8278074154191b3e94aa232
vanshikabansal02 <166854639+vanshikabansal02@users.noreply.github.com>
2025-03-29 01:22:57 +0530

    added

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,27 +1,27 @@
-<<<<<<< HEAD
+
 //import logInPopUp from './components/logInPopUp/logInPopUp'
 import Navbar from "./components/NavBar/Navbar";
 
 
-=======
-import logInPopUp from './components/logInPopUp/logInPopUp'
-import Navbar from 
->>>>>>> 82c6bccdbb09e97a6d25b2e3926b86b34e2b8b06
+
+import logInPopUp from './components/logInPopUp/logInPopUp';
+
+
 
 import './app.css'
 
 export function App() {
-  // const[showLogin,setShowLogin]=useState(false);
+   const[showLogin,setShowLogin]=useState(false);
 
   return (
     <>
         <div className="app">
           <Navbar></Navbar>
-          {/* {showLogin?<logInPopup/>:<></>}
+          { {showLogin?<logInPopup/>:<></>}
           <div className='app'>
             <Navbar setShowLogin={setShowLogin} />
             <p>hii</p>
-          </div> */}
+          </div> }
         </div>
     </>
   )
